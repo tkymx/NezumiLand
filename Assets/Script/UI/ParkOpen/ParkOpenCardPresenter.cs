3e97dc9ec1e359f4149e55b032609b61339a33cf
takuya watanabe <tkymx83@gmail.com>
2020-02-04 00:15:27 +0900

    カードを選択して、行動できる手前まで実装

--- Assets/Script/UI/ParkOpen/ParkOpenCardPresenter.cs
+++ Assets/Script/UI/ParkOpen/ParkOpenCardPresenter.cs
@@ -8,18 +8,76 @@ namespace NL
     {
         [SerializeField]
         private ParkOpenCardView ParkOpenCardView = null;
+        
+        public TypeObservable<int> OnTouchCardObservable { get; private set; }
+        public TypeObservable<int> OnCancelObservable { get; private set; }
+        public TypeObservable<int> OnUseObservable { get; private set; }
+
+        private bool isPrepareState = false;
 
         public void Initialize() {
             this.ParkOpenCardView.Initialize();
+
+            this.OnTouchCardObservable = new TypeObservable<int>();
+            this.OnCancelObservable = new TypeObservable<int>();
+            this.OnUseObservable = new TypeObservable<int>();
             this.disposables.Add(ParkOpenCardView.OnTouchCardObservable.Subscribe(_ => {
-                //TODO: タッチしたときの挙動を追加
+                this.OnTouchCardObservable.Execute(_);
             }));
+            this.disposables.Add(ParkOpenCardView.OnCancelObservable.Subscribe(_ => {
+                this.OnCancelObservable.Execute(_);
+            }));
+            this.disposables.Add(ParkOpenCardView.OnUseObservable.Subscribe(_ => {
+                this.OnUseObservable.Execute(_);
+            }));
+
             this.Close();
+            this.isPrepareState = false;
+        }
+
+        public void TogglePrepare()
+        {
+            if (this.isPrepareState) {
+                this.CancelPrepare();
+            }
+            else {
+                this.PrepareUsing();
+            }
+        }
+
+        public void PrepareUsing()
+        {
+            if (this.isPrepareState) {
+                return;
+            }
+
+            this.ParkOpenCardView.PlayAnimation(ParkOpenCardView.AnimationTag.ToLarge);
+            this.ParkOpenCardView.SetPrepareButtonVisible(true);
+            this.ParkOpenCardView.transform.SetAsLastSibling();
+            this.isPrepareState = true;
+        }
+
+        public void CancelPrepare()
+        {
+            if (!this.isPrepareState) {
+                return;
+            }
+
+            this.ParkOpenCardView.PlayAnimation(ParkOpenCardView.AnimationTag.ToSmall);
+            this.ParkOpenCardView.SetPrepareButtonVisible(false);
+            this.isPrepareState = false;
         }
 
         public void SetContents(ParkOpenCardModel parkOpenCardModel) {
             var sprite = ResourceLoader.LoadCardSprite(parkOpenCardModel.ImageName);
             this.ParkOpenCardView.UpdateView(parkOpenCardModel.Name, parkOpenCardModel.Description, sprite);            
+        }
+
+        public override void onPrepareShow()
+        {
+            this.isPrepareState = false;
+            this.ParkOpenCardView.SetPrepareButtonVisible(false);
+            this.ParkOpenCardView.PlayAnimation(ParkOpenCardView.AnimationTag.Initial);
         }
     }    
 }
--- Assets/Script/UI/ParkOpen/ParkOpenDeckPresenter.cs
+++ Assets/Script/UI/ParkOpen/ParkOpenDeckPresenter.cs
@@ -13,11 +13,61 @@ namespace NL
         [SerializeField]
         private ParkOpenCardPresenter parkOpenCardPresenter3 = null;
 
+        private ParkOpenCardPresenter GetParkOpenCardPresenter(PlayerParkOpenDeckModel.CountType countType)
+        {
+            if (countType == PlayerParkOpenDeckModel.CountType.First)
+            {
+                return parkOpenCardPresenter1;
+            }
+            if (countType == PlayerParkOpenDeckModel.CountType.Second)
+            {
+                return parkOpenCardPresenter2;
+            }
+            if (countType == PlayerParkOpenDeckModel.CountType.Third)
+            {
+                return parkOpenCardPresenter3;
+            }
+
+            return null;
+        }
+
         public void Initialize() {
             this.parkOpenCardPresenter1.Initialize();
             this.parkOpenCardPresenter2.Initialize();
             this.parkOpenCardPresenter3.Initialize();
             this.Close();
+
+            this.SetTouchEvent(PlayerParkOpenDeckModel.CountType.First);            
+            this.SetTouchEvent(PlayerParkOpenDeckModel.CountType.Second);            
+            this.SetTouchEvent(PlayerParkOpenDeckModel.CountType.Third);            
+        }
+
+        public void SetTouchEvent(PlayerParkOpenDeckModel.CountType countType)
+        {
+            var playerParkOpenDeckModel = GetParkOpenCardPresenter(countType);
+            this.disposables.Add(playerParkOpenDeckModel.OnTouchCardObservable.Subscribe(_ => {
+                CancelAnother(countType);
+                playerParkOpenDeckModel.TogglePrepare();
+            }));
+            this.disposables.Add(playerParkOpenDeckModel.OnCancelObservable.Subscribe(_ => {
+                playerParkOpenDeckModel.CancelPrepare();
+            }));
+            this.disposables.Add(playerParkOpenDeckModel.OnUseObservable.Subscribe(_ => {
+                // カードを消費する
+            }));
+        }
+
+        public void CancelAnother(PlayerParkOpenDeckModel.CountType countType)
+        {
+            if (countType != PlayerParkOpenDeckModel.CountType.First) {
+                GetParkOpenCardPresenter(PlayerParkOpenDeckModel.CountType.First).CancelPrepare();
+            }
+            if (countType != PlayerParkOpenDeckModel.CountType.Second) {
+                GetParkOpenCardPresenter(PlayerParkOpenDeckModel.CountType.Second).CancelPrepare();
+            }
+            if (countType != PlayerParkOpenDeckModel.CountType.Third) {
+                GetParkOpenCardPresenter(PlayerParkOpenDeckModel.CountType.Third).CancelPrepare();
+            }
         }
 
         public void SetContents(PlayerParkOpenDeckModel playerParkOpenDeckModel) {
